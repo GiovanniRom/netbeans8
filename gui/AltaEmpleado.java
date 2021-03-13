f1c34b8333adbef899ef0d24022cc404a40cf0ca
Giovanni Romero <garm9609@gmail.com>
2021-03-12 18:19:29 -0600

    proyecto casi terminado

--- gui/AltaEmpleado.java
+++ gui/AltaEmpleado.java
@@ -21,12 +21,14 @@ import mx.unam.aragon.fes.persistencia.ArchivoEmpleado;
  */
 public class AltaEmpleado extends javax.swing.JFrame {
 private ArrayList<Empleado> lista;
+private int indice;
     /**
      * Creates new form AltaEmpleado
      */
     public AltaEmpleado() {
         initComponents();
         lista = new ArrayList<Empleado>();
+        indice = 0;
     }
 
     /**
@@ -76,6 +78,7 @@ private ArrayList<Empleado> lista;
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
+        jLabel14 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -311,8 +314,18 @@ private ArrayList<Empleado> lista;
         });
 
         jButton2.setText("Cargar");
+        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton2MouseClicked(evt);
+            }
+        });
 
         jButton3.setText(">>");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
 
         jButton4.setText("<<");
 
@@ -330,6 +343,9 @@ private ArrayList<Empleado> lista;
                 jButton6MouseClicked(evt);
             }
         });
+
+        jLabel14.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel14.setText("0");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -352,13 +368,19 @@ private ArrayList<Empleado> lista;
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1)))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel14)
+                .addGap(405, 405, 405))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addComponent(jLabel14)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2)
@@ -432,7 +454,53 @@ private ArrayList<Empleado> lista;
         
     }//GEN-LAST:event_jButton1MouseClicked
 
-     
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+        // TODO add your handling code here:
+        int respuesta = JOptionPane.showConfirmDialog(this, "Esta accion borara los datos actuales. ¿Estas seguro de continuar?");
+        if(respuesta == 0){
+            //leer los datos del archivo
+            //subir al array list actual
+            //el primer elemento se pondra en el formulario
+            ArchivoEmpleado persistencia = new ArchivoEmpleado();
+            JFileChooser jfc = new JFileChooser();
+            jfc.showOpenDialog(this);
+            persistencia.setArchivo(jfc.getSelectedFile().getAbsolutePath());
+            this.lista = persistencia.leerEmpleado();
+            this.limpiarFormulario();
+            this.indice =0;
+            this.llenarFormulario(this.indice);
+            
+        }else{
+            
+        }
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        //falta hacer control de tamaño
+        if(this.indice<this.lista.size()-1){
+        this.indice ++;// this.indice = this.indice +1;
+        this.jLabel14.setText(""+this.indice);
+        llenarFormulario(indice);
+        }else{
+            this.indice = 0;
+        }
+         this.jLabel14.setText("" + this.indice);
+        llenarFormulario(indice);
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void llenarFormulario(int indice){
+        if(lista.size()>0){
+            Empleado e = lista.get(indice);
+            this.jTextField1.setText(e.getNombre());
+            this.jTextField2.setText(e.getApPaterno());
+            this.jTextField3.setText(e.getApMaterno());
+            this.jTextField4.setText(""+e.getEdad());
+            this.jTextField5.setText(e.getCurp());
+            //terminar el llenar formulario de tarea
+        }
+    } 
+    
+    
     private void limpiarFormulario(){
       this.jTextField1.setText("");
       this.jTextField2.setText("");
@@ -504,6 +572,7 @@ private ArrayList<Empleado> lista;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
